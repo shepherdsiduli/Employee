5aa73ef7964b6fc5df90ca0fdcda84d94e21f236
shepherdsiduli <shepherdsiduli@gmail.com>
2023-11-20 06:00:02 +0200

    UI fix

--- app/src/main/java/com/shepherd/employee/ui/screens/SuccessfulScreen.kt
+++ app/src/main/java/com/shepherd/employee/ui/screens/SuccessfulScreen.kt
@@ -1,10 +1,12 @@
 package com.shepherd.employee.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
@@ -12,8 +14,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.shepherd.employee.R
 import com.shepherd.employee.ui.navigation.Screen
@@ -39,6 +46,23 @@ fun SuccessfulScreen(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center,
         ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_success),
+                contentDescription = "Success Image",
+                modifier = Modifier
+                    .size(200.dp)
+                    .padding(16.dp),
+            )
+
+            Text(
+                text = stringResource(id = R.string.successful),
+                color = Color.Black,
+                modifier = Modifier.padding(bottom = 10.dp),
+                fontSize = 16.sp,
+                textAlign = TextAlign.Start,
+                fontWeight = FontWeight.Bold,
+            )
+
             Button(
                 onClick = {
                     viewModel.clearData()
